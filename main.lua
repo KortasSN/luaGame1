45f2665bee7b72c083f754d6a61f433277518c22
KortasSM <kortas@gmail.com>
2018-09-10 13:30:25 -0500

    added timer, score and gamestate

--- main.lua
+++ main.lua
@@ -5,32 +5,51 @@ function love.load()
   button.size = 50
 
   score = 0
-  time = 0
+  timer = 10
 
+  gameState = 1
   myFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
+  if gameState == 2 then
+    if timer > 0 then
+      timer = timer - dt
+    end
 
+    if timer < 0 then
+      timer = 0
+      gameState = 1
+      score = 0
+
+    end
+  end
 end
 
 function love.draw()
-
-  love.graphics.setColor(1, 0, 0)
-  love.graphics.circle("fill", button.x, button.y, button.size)
+  if gameState == 2 then
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", button.x, button.y, button.size)
+  end
 
   love.graphics.setFont(myFont)
   love.graphics.setColor(1, 1, 1)
   love.graphics.print(score)
+  love.graphics.print(math.ceil(timer), 100, 0)
 end
 
 function love.mousepressed( x, y, b, istouch)
-  if b == 1 then
+  if b == 1 and gameState == 2 then
     if distanceBetween(button.x, button.y, love.mouse.getX(), love.mouse.getY()) < button.size then
       score = score + 1
       button.x = math.random(button.size, love.graphics.getWidth() - button.size)
       button.y = math.random(button.size, love.graphics.getHeight() - button.size)
     end
+  end
+
+  if gameState == 1 then
+    gameState = 2
+    timer = 10
   end
 end
 
